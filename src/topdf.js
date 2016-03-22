c1fde190c9c551c4f4eb0702193bee381be10ae7
Hallvord Reiar Michaelsen Steen <hsteen@mozilla.com>
2016-03-22 22:42:12 +0100

    Refactor the code embedding images - hopefully more GC-friendly now

--- src/topdf.js
+++ src/topdf.js
@@ -18,6 +18,10 @@ var argv = require('yargs')
   .demand('outputfile')
   .describe('outputfile', 'Name of PDF-file to write to')
 
+  .demand('limits')
+  .describe('limits', 'How many domains to get (start,num)')
+  .default('limits', '0,50')
+
   .argv;
 
 var pdfdoc = new PDFDocument({title:"Compatipede report"});
@@ -30,16 +34,17 @@ var db = connection.database('compatipede-jobs-top-1000');
 var processed = {};
 var pendingDomains = 0, pendingDocs = 0, pendingAttachments = 0;
 var attachment_data = {}; // store attachment meta data per URI until we're ready to insert everything into the document
+var pendingUrls = [];
+var limits = argv.limits.split(/,/);
 
-db.view('jannahJobs/listDomains', {group: true, limit: 30}, function(dberr, res){
+db.view('jannahJobs/listDomains', {group: true, skip: limits[0], limit: limits[1]}, function(dberr, res){
   if(dberr){
     console.log(dberr);
     process.exit(1);
   }
   pendingDomains = res.length;
   for(var i = 0; i < res.length; i++){
-    var domain = res[i].key;
-    db.view('jannahJobs/byDomain', {key: domain}, function (dberr, docs) {
+    db.view('jannahJobs/byDomain', {key: res[i].key}, function (dberr, docs) {
       if(dberr){
         console.log(dberr);
       }
@@ -57,6 +62,7 @@ db.view('jannahJobs/listDomains', {group: true, limit: 30}, function(dberr, res)
           var variant_id = doc.jobDetails.targetURI + ', ' + doc.jobDetails.engine + ', ' + ua.family;
           var url = doc.jobDetails.targetURI;
           if(processed[url][variant_id]){
+            checkIfMetadataComplete();
             continue; // we already had one of those screenshots
           }
           //console.log(doc);
@@ -70,123 +76,76 @@ db.view('jannahJobs/listDomains', {group: true, limit: 30}, function(dberr, res)
             var data = doc._attachments[att_id].screenshot;
             // If we've processed all the meta data, we start fetching the attachments..
             // In other words, j is docs.length, doc is last document, domain is last domain..
-            if(pendingDomains === 0 && pendingDocs === 0){
-              startAddingAttachments();
-            }
+            checkIfMetadataComplete();
           }
         }else{
-          console.log('No attachments for ' + doc._id + ' domains to do: ' + pendingDomains+' docs to do: ' +pendingDocs + ' pending attchm ' + pendingAttachments);
-          if(pendingDomains === 0 && pendingDocs === 0){
-            startAddingAttachments();
-          }
+          console.log('No attachments for ' + doc._id + ' domains to do: ' + pendingDomains+' docs to do: ' +pendingDocs + ' attchm to do: ' + pendingAttachments);
+          checkIfMetadataComplete();
         }
       }
     });
   }
 });
 
-function startAddingAttachments(){
-  process.quit();
-  // URL by URL, we start requesting the attachments..
-  console.log('Here we go..');
-  for(var the_url in attachment_data){
-    var the_data = attachment_data[the_url];
-    console.log(the_url + ' ' + the_data.imgs.length);
-    the_data.loaded = 0;
-    the_data.imgs.forEach(function(imgdata, index){
-      console.log('Requesting ' + imgdata.doc+', '+imgdata.name);
-      db.getAttachment(imgdata.doc, imgdata.name, function(dberr, data){
-        pendingAttachments--;
-        //console.log(data)
-        if(dberr){
-          console.log(dberr);
-        }
-        this.data.imgs[this.idx].body = data.body;
-        this.data.loaded++;
-        if(this.data.loaded === this.data.imgs.length){
-          console.log('Will embed data for '+this.data.domain + ' (attachments left to process: ' + pendingAttachments + ')');
-          this.data.imgs = this.data.imgs.sort(function(a,b){return a.variant >= b.variant});
-          pdfdoc.font('Times-Roman', 16).text(this.data.domain).font('Times-Roman', 10);
-          pdfdoc.moveDown();
-          pdfdoc.moveDown();
-          pdfdoc.moveDown();
-          var ypos = pdfdoc.y, xpos = pdfdoc.x, width = parseInt( ( pdfdoc.page.width - (pdfdoc.x*2)  ) / this.data.imgs.length ) ;
-          for(var i = 0; i < this.data.imgs.length; i++){
-            pdfdoc.textAnnotation(xpos + (width * i), ypos-20, width, 20, this.data.imgs[i].variant.replace(this.url+', ', ''), {color:'#ccc', fontSize:'9px'});
-            try{
-              pdfdoc.image(this.data.imgs[i].body, xpos + (width * i), ypos, {width:width});
-            }catch(e){
-              console.log(e);
-            }
-          }
-          // free all that memory..
-          delete attachment_data[this.url];
-          delete this.data;
-          if(pendingAttachments === 0){
-            pdfdoc.end();
-          }
-        }else{
-          console.log('Not yet enough attachments loaded for ' + this.data.domain + ' ( ' + this.data.loaded + '/' + this.data.imgs.length+')' + '(attachments left to process: ' + pendingAttachments + ')');
-        }
-      }.bind({data:the_data, url:the_url, idx: index}));
-    });
+function checkIfMetadataComplete(){
+  if(pendingDomains === 0 && pendingDocs === 0){
+    pendingUrls = Object.keys(attachment_data);
+    startAddingAttachments();
   }
 }
 
-
-/*              db.getAttachment(doc._id, att_id, function(dberr, data){
-                var doc = this.doc;
-                var url = doc.jobDetails.targetURI;
-                console.log('attachment ' + pendingAttachments + '  ');
-                pendingAttachments--;
-                //console.log(data)
-                if(dberr){
-                  console.log(dberr);
-                }
-              // if we've processed all documents (pendingDocs and Domains  == 0),
-              // we can start going through attachment_data to figure out
-              // which ones are completely fetched, insert them
-              // and drop the data..
-                // we think all data was processed, attachment_data should be complete 
-                // (though not all attachments are downloaded yet - this is by design..)
-                for(var the_url in attachment_data){
-                  var the_data = attachment_data[the_url];
-                  // check if all attachments are here
-                  var all_attachments_loaded = true;
-                  the_data.imgs.forEach(function(data){
-                    all_attachments_loaded = all_attachments_loaded && ('data' in data);
-                  });
-                  if(all_attachments_loaded){ 
-                    // insert all
-                    pdfdoc.moveDown().font('Times-Roman', 13);
-                    pdfdoc.text(doc.jobDetails.domain);
-                    pdfdoc.moveDown().font('Times-Roman', 10);
-                    pdfdoc.text(the_data);
-                    pdfdoc.moveDown().font('Times-Roman', 13);
-                    the_data.imgs.forEach(function(imgdata){
-                      
-                    });
-                    // free up some memory
-                    delete attachment_data[the_url];
-                  }
-                }
-              }else{
-                attachment_data[url].data = data.body;
-                pdfdoc.image(data.body);
-              }
-              if(pendingDocs === 0 && pendingAttachments === 0){
-                console.log('finish');
-                pdfdoc.end();
-              }
-            }.bind({doc:doc}));
+function startAddingAttachments(){
+  // URL by URL, we start requesting the attachments..
+  var the_url = pendingUrls.shift();
+  if(the_url && attachment_data[the_url]){
+    console.log(the_url + ' ' + attachment_data[the_url].imgs.length);
+  }else{
+    pdfdoc.end();
+    return;
+  }
+  var this_data = {};
+  attachment_data[the_url].loaded = 0;
+  attachment_data[the_url].imgs.forEach(function(imgdata, index){
+    console.log('Requesting ' + imgdata.doc+', '+imgdata.name);
+    db.getAttachment(imgdata.doc, imgdata.name, (function(dberr, data){
+      pendingAttachments--;
+      //console.log(data)
+      if(dberr){
+        console.log(dberr);
+      }
+      this.data_store[this.idx] = data.body;
+      this.data.loaded++;
+      if(this.data.loaded === this.data.imgs.length){
+        setTimeout(startAddingAttachments, 5); // Trigger another round later on..
+        console.log('Will embed data for '+this.data.domain + ' (attachments left to process: ' + pendingAttachments + ')');
+        this.data.imgs = this.data.imgs.sort(function(a,b){return a.variant >= b.variant});
+        pdfdoc.font('Times-Roman', 16).text(this.data.domain).font('Times-Roman', 10);
+        pdfdoc.moveDown();
+        pdfdoc.moveDown();
+        pdfdoc.moveDown();
+        var ypos = pdfdoc.y, xpos = pdfdoc.x, width = parseInt( ( pdfdoc.page.width - (pdfdoc.x*2)  ) / this.data.imgs.length ) ;
+        for(var i = 0; i < this.data.imgs.length; i++){
+          pdfdoc.textAnnotation(xpos + (width * i), ypos-20, width, 20, this.data.imgs[i].variant.replace(this.url+', ', ''), {color:'#ccc', fontSize:'9px'});
+          try{
+            pdfdoc.image(this.data_store[i], xpos + (width * i), ypos, {width:width});
+            this.data.imgs[i] = null; // we'll try to make GC happen as soon as possible
+          }catch(e){
+            console.log(e);
           }
         }
+        // Try to let V8 free all that memory..
+        delete attachment_data[this.url];
+        delete this.data;
+        delete this.data_store;
+        if(pendingAttachments === 0){
+          pdfdoc.end();
+        }
+      }else{
+        console.log('Not yet enough attachments loaded for ' + this.data.domain + ' ( ' + this.data.loaded + '/' + this.data.imgs.length + ', attachments left to process: ' + pendingAttachments + ')');
       }
-    });
-  }
-
-});*/
-
+    }).bind({data:attachment_data[the_url], url:the_url, idx: index, data_store: this_data}));
+  });
+}
 
 stream.on('finish', function() {
   console.log('DONE');
